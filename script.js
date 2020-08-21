960faf0c589696253e534688faef5d1f4b92b853
Alex-Peeters1969 <67190405+Alex-Peeters1969@users.noreply.github.com>
2020-08-21 12:01:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let person ={
+    name: 'Alex',
+    age: 50
+};
+
+console.log (person.name , person.age);
+
+let evaluations ={
+    value: [7,10,9]
+};
+
+console.log (evaluations.value);
+
+let selectorcolors =["groen", "blauw", "rood"];
+
+console.log (selectorcolors.length);
+console.log (selectorcolors [0]);
+console.log (selectorcolors [2]);
+console.log (selectorcolors.push ('geel'));
+console.log (selectorcolors.push ('5'));
+console.log (selectorcolors.push ('greetings:ik ben een object'));    
+console.log (selectorcolors [5]);
